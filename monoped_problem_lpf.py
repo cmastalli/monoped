702f88e9495b3ef2e68ab382a385a1dfa6473411
gfadini <gabfadini@gmail.com>
2020-09-15 14:50:59 +0200

    [wip] added cost, velocity and frame bounds to LPF

--- monoped_problem_lpf.py
+++ monoped_problem_lpf.py
@@ -17,6 +17,9 @@ dt = conf.dt
 # MONOPED MODEL
 # Create the monoped and actuator
 monoped = monoped.createMonopedWrapper(nbJoint = conf.n_links, linkLength=0.16, floatingMass=0.37, linkMass=0.1)
+# monoped = monoped.createSoloTBWrapper()
+# import slice_model
+# monoped = slice_model.loadSoloLeg(solo8 = True)
 robot_model = monoped.model
 state = crocoddyl.StateMultibody(robot_model)
 motor_mass, n_gear, lambda_l = [np.array([53e-3] * conf.n_links), np.array([9] * conf.n_links), np.array([1] * (conf.n_links + 1))]
@@ -25,6 +28,8 @@ robot_model.effortLimit = 2.5 * np.ones(3)
 #robot_model.T_mu = np.zeros(conf.n_links)
 state.robot_model = robot_model
 
+# ACTUATION TYPE
+# actuation = crocoddyl.ActuationModelFull(state)
 actuation = crocoddyl.ActuationModelFloatingBase(state)
 
 q0 = np.zeros(1 + 2 * conf.n_links)
@@ -80,6 +85,31 @@ bounds = crocoddyl.ActivationBounds(np.concatenate([np.zeros(1), -1e3* np.ones(s
 stateAct = crocoddyl.ActivationModelWeightedQuadraticBarrier(bounds, np.concatenate([np.ones(1), np.zeros(state.nx - 1)]))
 nonPenetration = crocoddyl.CostModelState(state, stateAct, np.zeros(state.nx), actuation.nu)
 
+# Changing to frame penalization
+important_frames = ['base', 'revolute_1', 'revolute_2', 'foot']
+groundLine = np.zeros(3)
+groundBounds = crocoddyl.ActivationBounds(np.zeros(3), 1e3*np.ones(3))
+groundAct = crocoddyl.ActivationModelWeightedQuadraticBarrier(groundBounds, np.concatenate([np.zeros(2), np.ones(1)]))
+
+footGroundRef = crocoddyl.FrameTranslation(footFrameID, groundLine)
+footGroundCost = crocoddyl.CostModelFrameTranslation(state, groundAct, footGroundRef, actuation.nu)
+
+rev1GroundRef = crocoddyl.FrameTranslation(robot_model.getFrameId('revolute_1'), groundLine)
+rev1GroundCost = crocoddyl.CostModelFrameTranslation(state, groundAct, rev1GroundRef, actuation.nu)
+
+rev2GroundRef = crocoddyl.FrameTranslation(robot_model.getFrameId('revolute_2'), groundLine)
+rev2GroundCost = crocoddyl.CostModelFrameTranslation(state, groundAct, rev1GroundRef, actuation.nu)
+
+maxVelocity = np.concatenate([np.zeros(state.nq + 1), robot_model.velocityLimit])
+velocityBounds = crocoddyl.ActivationBounds(-maxVelocity, maxVelocity, 0.05)
+velocityAct = crocoddyl.ActivationModelWeightedQuadraticBarrier(velocityBounds, np.concatenate([np.zeros(state.nq + 1), np.ones(state.nv - 1)]))
+velocityCost = crocoddyl.CostModelState(state, velocityAct, np.zeros(state.nx), actuation.nu)
+
+maxTorque = robot_model.effortLimit[-actuation.nu:]
+torqueBounds = crocoddyl.ActivationBounds(-maxTorque, maxTorque, 0.8)
+torqueAct = crocoddyl.ActivationModelWeightedQuadraticBarrier(torqueBounds, np.ones(actuation.nu))
+torqueCost = crocoddyl.CostModelControl(state, torqueAct, actuation.nu)
+
 # MAXIMIZATION
 jumpBounds = crocoddyl.ActivationBounds(-1e3*np.ones(state.nx), np.concatenate([np.zeros(1), +1e3* np.ones(state.nx-1)]))
 jumpAct = crocoddyl.ActivationModelWeightedQuadraticBarrier(bounds, np.concatenate([-np.ones(1), np.zeros(state.nx - 1)]))
@@ -110,10 +140,16 @@ frictionCone = crocoddyl.CostModelContactFrictionCone(state,
 # Creating the action model for the KKT dynamics with simpletic Euler integration scheme
 contactCostModel = crocoddyl.CostModelSum(state, actuation.nu)
 # contactCostModel.addCost('frictionCone', frictionCone, 1e-6)
-contactCostModel.addCost('joule_dissipation', joule_dissipation, 5e-3)
-contactCostModel.addCost('joint_friction', joint_friction, 5e-3)
-contactCostModel.addCost('velocityRegularization', v2, 1e-1)
-contactCostModel.addCost('nonPenetration', nonPenetration, 1e5)
+contactCostModel.addCost('joule_dissipation', joule_dissipation, 5e-2)
+contactCostModel.addCost('joint_friction', joint_friction, 5e-2)
+# contactCostModel.addCost('velocityRegularization', v2, 1e-1)
+contactCostModel.addCost('velocityBound', velocityCost, 1e-1)
+contactCostModel.addCost('torqueBound', torqueCost, 1e0)
+# contactCostModel.addCost('nonPenetration', nonPenetration, 1e5)
+contactCostModel.addCost('footNP', footGroundCost, 1e5)
+contactCostModel.addCost('rev1NP', rev1GroundCost, 1e5)
+contactCostModel.addCost('rev2NP', rev2GroundCost, 1e5)
+
 contactDifferentialModel = crocoddyl.DifferentialActionModelContactFwdDynamics(state,
         actuation,
         contactModel,
@@ -122,18 +158,27 @@ contactDifferentialModel = crocoddyl.DifferentialActionModelContactFwdDynamics(s
         True) # bool enable force
 contactPhase = IntegratedActionModelLPF(contactDifferentialModel, dt)
 
-runningCostModel.addCost("joule_dissipation", joule_dissipation, 5e-3)
-runningCostModel.addCost('joint_friction', joint_friction, 5e-3)
-runningCostModel.addCost("velocityRegularization", v2, 1e0)
-runningCostModel.addCost("nonPenetration", nonPenetration, 1e6)
+runningCostModel.addCost("joule_dissipation", joule_dissipation, 5e-2)
+runningCostModel.addCost('joint_friction', joint_friction, 5e-2)
+# runningCostModel.addCost("velocityRegularization", v2, 1e-2)
+runningCostModel.addCost('velocityBound', velocityCost, 1e-1)
+runningCostModel.addCost('torqueBound', torqueCost, 1e0)
+# runningCostModel.addCost("nonPenetration", nonPenetration, 1e6)
+runningCostModel.addCost('footNP', footGroundCost, 1e5)
+runningCostModel.addCost('rev1NP', rev1GroundCost, 1e5)
+runningCostModel.addCost('rev2NP', rev2GroundCost, 1e5)
 # runningCostModel.addCost("maxJump", maximizeJump, 1e2)
 terminalCostModel.addCost("footPose", footTrackingCost, 5e3)
 # terminalCostModel.addCost("footVelocity", footFinalVelocity, 1e0)
 
 runningModel = IntegratedActionModelLPF(
     crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actuation, runningCostModel), dt)
-runningModel.set_alpha(1e1)
+runningModel.set_alpha(5e-1)
 terminalModel = IntegratedActionModelLPF(crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actuation, terminalCostModel), 0.)
+
+# setting the limits not used
+# runningModel.u_lb = -robot_model.effortLimit[-actuation.nu:]
+# terminalModel.u_ub = robot_model.effortLimit[-actuation.nu:]
 
 # Setting the nodes of the problem with a sliding variable
 ratioContactTotal = 0.4/(conf.dt*T) # expressed as ratio in [s]
@@ -142,7 +187,6 @@ flyingNodes = conf.T - contactNodes
 problem_with_contact = crocoddyl.ShootingProblem(x0,
                                                 [contactPhase] * contactNodes + [runningModel] * flyingNodes,
                                                 terminalModel)
-problem_without_contact = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
 
 # SOLVE
 ddp = crocoddyl.SolverFDDP(problem_with_contact)
@@ -150,7 +194,7 @@ ddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose(),])
 ddp.lpf=True
 # Additionally also modify ddp.th_stop and ddp.th_grad
 ddp.th_stop = 1e-6
-ddp.solve([],[], maxiter = int(1e3))
+ddp.solve([],[], maxiter = int(1e2))
 ddp.robot_model = robot_model
 
 # SHOWING THE RESULTS
@@ -167,6 +211,7 @@ class extractDDPLPF():
 
 ddpLPF = extractDDPLPF(ddp, actuation.nu)
 
+plotOCSolution(ddpLPF)
 plot_frame_trajectory(ddpLPF, [frame.name for frame in ddpLPF.robot_model.frames], trid = False)
 animateMonoped(ddpLPF, saveAnimation=False)
 plot_power(ddpLPF)
